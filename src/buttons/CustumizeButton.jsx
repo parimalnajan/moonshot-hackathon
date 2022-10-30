2cfb58bea616e5e3cd6b48f8244eb44857b73048
Parimal Najan <63017672+parimalnajan@users.noreply.github.com>
2022-10-30 11:40:31 +0530

    added examples

--- src/buttons/CustumizeButton.jsx
+++ src/buttons/CustumizeButton.jsx
@@ -6,51 +6,129 @@
 
 
 const CustumizeButton = () => {
-  const [buttonStyles,setbuttonStyles] = useState({color:"red",backgroundColor:"gray"});
-
+  const [buttonStyles,setbuttonStyles] = useState({color:"red",backgroundColor:"gray",padding:"4px"});
+  
+  
+  
   const button = `
- import React, { useState } from 'react'
+  import React, { useState } from 'react'
+  
+  export default function App() {
+    const [buttonStyles,setbuttonStyles] = useState({color:"red",backgroundColor:"gray"});
+    
+    return <button style={buttonStyles}>
+    button
+    </button>
+  }        `
+
+  const list = `import React, { useState } from 'react'
 
   export default function App() {
-  const [buttonStyles,setbuttonStyles] = useState({color:"red",backgroundColor:"gray"});
 
-    return <button style={buttonStyles}>
-  button
-</button>
-}`        
+    return(
+      <ul>
+      <li>
+        one
+      </li>
+      <li>two    
+      </li>
+    </ul>
+    )
+   
+  }`;
+
+  const form = `import React, { useState } from 'react'
+
+  export default function App() {
+    return(
+      <div>
+      <label for="name">Name (4 to 8 characters):</label>
+  
+      <input type="text" id="name" name="name" required
+             minlength="4" maxlength="8" size="10"/>
+             </div>
+    )
     
+}
+`;
+
+  
+  const [editorState, setEditorState] = useState(button)
       const styleKeys= Object.keys(buttonStyles)
-      console.log(styleKeys)
+      console.log(button)
       
       return (<div>
         <div style={{padding:"16px", marginLeft:"24px"}}>
-          </div><Sandpack files={{"/App.js":button}} template="react" SandpackPredefinedTheme = "dark" theme={"dark"} />;
+          </div><Sandpack files={{"/App.js":editorState}} template="react" SandpackPredefinedTheme = "dark" theme={"dark"} />;
 
-        <button style={buttonStyles}>
-          button
-        </button>
 
-        <div style={{display:"flex", flexDirection:"row"}}>
 
-        <div style={{display:"flex", flexDirection:"column"}}>
 
-          -----inputs here----
-          --input
-          padding x,  - +/- button - every input linked to state
-          padding y,
-          bg color
+
+        <div>component templates -click to open in editor-</div>
+          <button style={buttonStyles}>button</button>
+          <button
+            onClick={() => {
+              setEditorState(button);
+            }}
+          >
+            edit
+          </button>
+
+          <br />
+          <br />
+          <button style={{ padding: "4px" }}>- input form -</button>
+          <button
+            onClick={() => {
+              setEditorState(form);
+            }}
+          >
+            edit
+          </button>
+
+          <br />
+          <br />
+          <button style={{ padding: "4px" }}>- lists -</button>
+          <button
+            onClick={() => {
+              setEditorState(list);
+            }}
+          >
+            edit
+          </button>
+
+          <br />
+          <br />
+          <button style={{ padding: "4px" }}>- nav elements -</button>
+          <button
+            onClick={() => {
+              setEditorState(button);
+            }}
+          >
+            edit
+          </button>
+
+          <br />
+          <br />
+          <div>Style snippet: </div>
+          <div
+            style={{
+              border: "1px solid silver",
+              display: "flex",
+              flexDirection: "column",
+              padding: "16px",
+            }}
+          >
+            <div>
+              {" "}
+              {styleKeys.map((x, i) => (
+                <div key={i}>
+                  {x}:{buttonStyles[x]}
+                </div>
+              ))}
+            </div>
           </div>
-          
-           </div>
-
-           <br/>
-           <br/>
-        <div style={{border:"1px solid silver", display:"flex", flexDirection:'column'}}> 
-        <div>Style snippet: </div>
-          <div> {styleKeys.map((x,i)=><div key={i} >{x}:{buttonStyles[x]}</div>)}</div>
-           </div>
-        
-    </div>
+        </div>
       );
     }
     
